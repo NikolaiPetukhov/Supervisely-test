90bb622eb51622a65d43cc24376071d455a68fe5
Nikolai Petukhov <nikolai.petukhov@ioet.com>
2022-12-01 01:14:36 -0500

    merged with cli

--- PD-3/merge_videos.py
+++ PD-3/merge_videos.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import cv2 as cv
 import numpy as np
@@ -99,3 +100,22 @@ def merge(videos_path, save_path, ratio='16:9'):
     for v in videos:
         v.cap.release()
     out.release()
+    
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('videos_path', nargs=1, help='Path to video directory')
+    parser.add_argument('save_path', nargs=1, help='Path to save video')
+    parser.add_argument('-r', '--ratio', default='16:9', 
+        help='dimensions ratio of resulting video')
+    args = parser.parse_args()
+    videos_path = args.videos_path[0]
+    save_path = args.save_path[0]
+    ratio = args.ratio
+    print('Merging...')
+    merge(videos_path, save_path, ratio)
+    print('Done!')
+    
+
+if __name__ == "__main__":
+    run()
